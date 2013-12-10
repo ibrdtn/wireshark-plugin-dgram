50d930a20b82409f7990aad48af83f7b9c0e6df2
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-12-10 10:39:02 +0100

    Add lowpan suffix to variables and methods

--- src/packet-dgram.c
+++ src/packet-dgram.c
@@ -15,10 +15,10 @@
 
 static dissector_handle_t data_handle;
 
-static int proto_dgram = -1;
-static int hf_dgram_seqno = -1;
-static int hf_dgram_first_frame = -1;
-static int hf_dgram_last_frame = -1;
+static int proto_dgram_lowpan = -1;
+static int hf_dgram_lowpan_seqno = -1;
+static int hf_dgram_lowpan_first_frame = -1;
+static int hf_dgram_lowpan_last_frame = -1;
 static gint ett_dgram = -1;
 
 static const value_string frame_type_vals[] = {
@@ -29,14 +29,16 @@ static const value_string frame_type_vals[] = {
     {0, NULL}
 };
 
-static void        dissect_dgram            (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
-static void        dissect_dgram_fh         (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint *offset);
+static void        dissect_dgram_lowpan     (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+static void        dissect_dgram_lowpan_fh  (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint *offset);
+
+
 static void        proto_init_dgram         (void);
 void               proto_register_dgram     (void);
 void               proto_reg_handoff_dgram  (void);
 
 static void
-dissect_dgram(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+dissect_dgram_lowpan(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
 	tvbuff_t *payload_tvb = NULL;
 
@@ -52,11 +54,11 @@ dissect_dgram(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
 		guint offset = 0;
 
-		proto_root = proto_tree_add_protocol_format(tree, proto_dgram, tvb, 0, tvb_length(tvb), "LowPAN Datagram");
+		proto_root = proto_tree_add_protocol_format(tree, proto_dgram_lowpan, tvb, 0, tvb_length(tvb), "LowPAN Datagram");
 		dgram_tree = proto_item_add_subtree(proto_root, ett_dgram);
 
 		// dissect frame header
-		dissect_dgram_fh(tvb, pinfo, dgram_tree, &offset);
+		dissect_dgram_lowpan_fh(tvb, pinfo, dgram_tree, &offset);
 
 		payload_tvb = tvb_new_subset_remaining(tvb, offset);
 
@@ -67,7 +69,7 @@ dissect_dgram(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
 /** parse the frame header **/
 static void
-dissect_dgram_fh(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint *offset)
+dissect_dgram_lowpan_fh(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint *offset)
 {
 	if (tree) { /* we are being asked for details */
 		proto_item *ti = NULL;
@@ -87,13 +89,13 @@ dissect_dgram_fh(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint *off
 		dgram_tree = proto_item_add_subtree(ti, ett_dgram);
 
 		// sequence number
-		proto_tree_add_item(dgram_tree, hf_dgram_seqno, tvb, *offset, 1, ENC_NA);
+		proto_tree_add_item(dgram_tree, hf_dgram_lowpan_seqno, tvb, *offset, 1, ENC_NA);
 
 		// first frame
-		proto_tree_add_item(dgram_tree, hf_dgram_first_frame, tvb, *offset, 1, ENC_NA);
+		proto_tree_add_item(dgram_tree, hf_dgram_lowpan_first_frame, tvb, *offset, 1, ENC_NA);
 
 		// last frame
-		proto_tree_add_item(dgram_tree, hf_dgram_last_frame, tvb, *offset, 1, ENC_NA);
+		proto_tree_add_item(dgram_tree, hf_dgram_lowpan_last_frame, tvb, *offset, 1, ENC_NA);
 
 		if ((header & (0x02 << 4)) && !(header & (0x01 << 4))) {
 			// beacon
@@ -108,13 +110,13 @@ dissect_dgram_fh(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint *off
 }
 
 static gboolean
-dissect_dgram_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
+dissect_dgram_lowpan_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 {
 	const char header = tvb_get_guint8(tvb, 0);
 
 	/** dissect the frames **/
 	if ((header && (0x03 << 6)) > 0) {
-		dissect_dgram(tvb, pinfo, tree);
+		dissect_dgram_lowpan(tvb, pinfo, tree);
 		return TRUE;
 	}
 
@@ -125,15 +127,15 @@ void
 proto_register_dgram(void)
 {
 	static hf_register_info hf[] = {
-		{ &hf_dgram_seqno,
+		{ &hf_dgram_lowpan_seqno,
 			{ "Sequence number", "dgram.seqno",
 				FT_UINT8, BASE_DEC, NULL, (0x03 << 2), NULL, HFILL }
 		},
-		{ &hf_dgram_first_frame,
+		{ &hf_dgram_lowpan_first_frame,
 			{ "First Frame", "dgram.first",
 				FT_BOOLEAN, 8, NULL, (0x02), NULL, HFILL }
 		},
-		{ &hf_dgram_last_frame,
+		{ &hf_dgram_lowpan_last_frame,
 			{ "Last Frame", "dgram.last",
 				FT_BOOLEAN, 8, NULL, (0x01), NULL, HFILL }
 		}
@@ -146,17 +148,17 @@ proto_register_dgram(void)
 
 	register_init_routine(proto_init_dgram);
 
-	proto_dgram = proto_register_protocol(
+	proto_dgram_lowpan = proto_register_protocol(
 		"LowPAN Datagram Protocol", /* name */
 		"dgram:lowpan", /* short name */
 		IEEE802154_PROTOABBREV_DGRAM /* abbrev */
 	);
 
-    proto_register_field_array(proto_dgram, hf, array_length(hf));
+    proto_register_field_array(proto_dgram_lowpan, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
 
 	/*  Register dissectors with Wireshark. */
-	register_dissector(IEEE802154_PROTOABBREV_DGRAM, dissect_dgram, proto_dgram);
+	register_dissector(IEEE802154_PROTOABBREV_DGRAM, dissect_dgram_lowpan, proto_dgram_lowpan);
 }
 
 void
@@ -168,16 +170,16 @@ proto_reg_handoff_dgram(void)
 
 	if (!prefs_initialized){
 		/* Get the dissector handles. */
-		dgram_handle   = create_dissector_handle(dissect_dgram, proto_dgram);
+		dgram_handle   = create_dissector_handle(dissect_dgram_lowpan, proto_dgram_lowpan);
 		data_handle    = find_dissector("data");
 		prefs_initialized = TRUE;
 	} else {
 		// Nothing to deregister.
-		heur_dissector_delete(IEEE802154_PROTOABBREV_WPAN, dissect_dgram_heur, proto_dgram);
+		heur_dissector_delete(IEEE802154_PROTOABBREV_WPAN, dissect_dgram_lowpan_heur, proto_dgram_lowpan);
 	}
 
 	/* Register our dissector with IEEE 802.15.4 */
-	heur_dissector_add(IEEE802154_PROTOABBREV_WPAN, dissect_dgram_heur, proto_dgram);
+	heur_dissector_add(IEEE802154_PROTOABBREV_WPAN, dissect_dgram_lowpan_heur, proto_dgram_lowpan);
 }
 
 static void
